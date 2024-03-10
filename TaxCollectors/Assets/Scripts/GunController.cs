5d4851cf9f2486802a94399ee5cd26d15526145f
CJPhill <cphill39@lion.lmu.edu>
2024-03-09 17:00:23 -0800

    base gun functionality working

--- TaxCollectors/Assets/Scripts/GunController.cs
+++ TaxCollectors/Assets/Scripts/GunController.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
+    public Transform firePoint; // The point from where the projectile will be fired
+    public GameObject bulletPrefab; // The projectile prefab
+    public float bulletForce = 10f; // The speed of the projectile
+
+    private Vector2 mousePos;
+
     private void Update()
     {
         LookAtMouse();
+        if (Input.GetMouseButtonDown(0)) // Check for left mouse button click
+        {
+            Shoot();
+        }
     }
 
     private void LookAtMouse()
@@ -14,4 +24,12 @@ public class GunController : MonoBehaviour
         Vector2 mousePos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.up = (Vector2)(mousePos - new Vector2(transform.position.x, transform.position.y));
     }
+
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+    
 }
